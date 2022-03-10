c04d1e94ec20f4281e284c012b7f4eff6ee2cac2
EnriqueSixto <cv4713@innovaccion.mx>
2022-03-10 11:02:59 -0600

    second commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,8 +5,7 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  if(x > y) return x
-  else if(x === y) return x
+  if(x === y || x > y) return x
   else{
     return y
   }
@@ -85,15 +84,16 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(numero === 10 || numero === 5) return true
-  else
-  return false
+  //operador ternario:
+  //numero === 10 || numero === 5 ? true: false
+  return numero === 10 || numero === 5
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  
   if(numero < 50 && numero > 20) return true
   else
   return false
@@ -107,9 +107,15 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if(numero === Math.floor(numero)) return true
+  //first solution
+  if(numero - Math.floor(numero) === 0) return true
   else
-  return false
+    return false
+  /* Second Solution
+   if(numero % 1 === 0) return true
+   else
+    return true
+   */
 }
 
 function fizzBuzz(numero) {
@@ -131,18 +137,10 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  
-  if(num1 < 0 || num2 < 0 || num3 < 0) return "Hay negativos"
-  else if(num1 === 0 || num2 === 0 || num3 === 0) return "Error"
-  else if(num3 > num1 && num3 > num2)
-  {
-    num3 ++;
-    return num3
-  }
-  else if(num1 > 0)
-  {
-    if(num1 > num2 && num1 > num3) return "Número 1 es mayor y positivo"
-  }
+  if(num1 === 0 || num2 === 0 || num3 === 0) return "Error"
+  else if(num1 < 0 || num2 < 0 || num3 < 0) return "Hay negativos"
+  else if(num1 > num2 && num1 > num3) return "Número 1 es mayor y positivo"
+  else if(num3 > num1 && num3 > num2) return num3 + 1
   else
     return false 
 }
@@ -153,19 +151,19 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if(numero === 0 || numero === 1) return false
-  else if(numero %2 === 1) return true
-  else
-    return false
-
-  
+  if(numero === 0 || numero === 1 || numero === 4) return false
+  for(let i= 2; i < numero / 2; i++)
+  {
+    if(numero % i === 0) return false
+  }
+  return true
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if(valor == true) return "Soy verdadero"
+  if(valor) return "Soy verdadero"
   else
     return "Soy falso"
 
@@ -175,22 +173,39 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  for(let i =0; i<11;i++)
+  var aux = []
+  for(let i = 0; i< 11; i++)
   {
-    return 6*i
+    
+    aux.push(6*i)
+    
   }
+  return aux
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if(numero > 99 && numero < 1000) return true
+  else
+    return false
+  /**
+   var aux = numero.toString()
+   if(aux.length === 3) return true
+   else return false
+   */
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  let contador = 0;
+  do{
+    contador ++;
+    numero +=5;
+  }while(contador < 8);
+  return numero
 }
 
 
