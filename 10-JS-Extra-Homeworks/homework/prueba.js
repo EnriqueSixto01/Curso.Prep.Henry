a5cb1e8c154a501fd9af962f1b1946bb3e0a696e
EnriqueSixto <cv4713@innovaccion.mx>
2022-03-12 20:58:51 -0600

    extra

---
+++ 10-JS-Extra-Homeworks/homework/prueba.js
@@ -0,0 +1,21 @@
+function deObjetoAmatriz(objeto){
+    // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
+    // un par clave-valor en forma de matriz.
+    //Ejemplo: 
+    /*objeto({
+        D: 1,
+        B: 2,
+        C: 3
+      }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
+    //Escribe tu código aquí
+    
+    const entries = Object.entries(objeto);
+    return entries;
+  }
+
+  const musica ={
+    Artista: "Celtian",
+    Genero: "Folk Metal"
+  };
+
+  console.log(deObjetoAmatriz(musica))
