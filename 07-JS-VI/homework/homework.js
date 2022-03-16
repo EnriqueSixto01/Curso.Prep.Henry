93a18a3cc11afb5ad33e52dd1e5629da43a66f9e
EnriqueSixto <cv4713@innovaccion.mx>
2022-03-16 00:38:30 -0600

    sixth commit

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,21 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb(); //dentro de el argumento cb que se pasa como argumento a invocarCallback, hay otra funcion.
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +26,26 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let sum =0;
+  for(let i=0;i<numeros.length;i++)
+  {
+    sum += numeros[i];
+  }
+
+  cb(sum);
+  
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  //let array = [1,2,3,4,5]
+  array.forEach(function(num){
+    cb(num);
+
+  });
+
 }
 
 function map(array, cb) {
@@ -35,12 +53,25 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+ let newArr = [];
+ for(let i=0;i<array.length;i++)
+ {
+   newArr.push(cb(array[i]))
+ }
+ return newArr;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  newArr = [];
+  for(let i=0;i<array.length;i++)
+  {
+    if(array[i].charAt(0) === 'a')
+    newArr.push(array[i])
+  }
+  return newArr;
 }
 
 // No modificar nada debajo de esta línea
---
+++ 07-JS-VI/homework/pruebas.js
@@ -0,0 +1,67 @@
+const { numeroMasGrande } = require("../../04-JS-III/homework/homework");
+
+const string = "enrique";
+
+console.log(string.charAt(0).toUpperCase() + string.slice(1));
+
+console.log(string.slice(-4,-1))
+/**************************************************************************/
+
+function operacionMatematica(n1, n2, cb) {
+    //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
+    //Devolver el callback pasándole como argumentos los números recibidos.
+    //Tu código:
+    return cb(n1,n2);
+  }
+
+function multiplicarDosNumeros(n1,n2)
+{
+    return n1 * n2;
+}
+
+function dividieDosNumeros(n1,n2)
+{
+    return n1 / n2;
+}
+console.log(operacionMatematica(10,85,multiplicarDosNumeros)); //llamamos a la funcion que realiza la tarea de callback
+                                                               //indicandole en su parametro la llamada a otra funcion
+console.log(operacionMatematica(10,85,dividieDosNumeros));
+
+/********************************************ForEach() ***************************************/
+
+const arrNum = [1,2,3,4,5];
+
+//Primero necesitamos una funcion callback (o funcion anonima)
+arrNum.forEach(function(num){      //Debe tomar al menos un parametro que represente los elementos del arreglo
+    console.log(num);
+});
+
+
+//con la representacion de la funcion flecha queda de la siguiente manera
+arrNum.forEach(num => console.log(num));
+
+//con parametros opcionales
+arrNum.forEach((num, index) => {
+    console.log('Indice: ' + index + ' Valor: ' + num);
+});
+
+
+/********************************************** Array.map() *************************************/
+let arr = [2,4,6,8];
+
+let modifiedArr = arr.map(function(element){   /*La function() callback es llamada en cada elemento del arreglo, 
+                                                y el método map() siempre le pasa el element actual,*/                      
+    return element * 3;                         //el index del elemento actual y el objeto array completo.
+});
+
+console.log(modifiedArr); //mostramos el nuevo arreglo con los elemntos multiplicados por 3
+
+
+let arr2 = [2, 3, 5, 7]
+
+arr2.map(function(element, index, array){
+    console.log(element);
+    console.log(index);
+    console.log(array);
+    return element;
+}, 80);
